ff3b6cedc2027752742de07e4910687858bbedff
kkoblog <koba0721a@gmail.com>
2025-03-24 16:33:41 +0900

    feat: トップページを/instagram-course-lpにリダイレクトするように設定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/instagram-course-lp',
+        permanent: true,
+      },
+    ];
+  },
   experimental: {
     esmExternals: 'loose'
   },
@@ -7,6 +16,11 @@ const nextConfig = {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
     return config;
   },
+  output: 'export',  // 静的エクスポートの場合
+  basePath: '',      // もしサブディレクトリにデプロイする場合は設定
+  images: {
+    unoptimized: true  // 静的エクスポートの場合は必要
+  }
 };
 
 module.exports = nextConfig;
